4ddc59948248b1f2b1d04c35e3c64a7959009f51
Gibirizon <swojteksz@gmail.com>
2024-06-24 19:09:00 +0200

    Added counter

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -2,5 +2,23 @@
 	<main>
 		<img src="/logo2.svg" alt="DFINITY logo" />
 		<h1>Siema!</h1>
+		<br />
+		<button @click="podbij">podbij</button>
+		{{ licznik }}
 	</main>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			licznik: 0,
+		}
+	},
+	methods: {
+		podbij() {
+			this.licznik++
+		},
+	},
+}
+</script>
